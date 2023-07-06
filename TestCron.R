0801e80fb4cbb2ec6483c7d8706bdf780a372e44
michael j Swallower <nickamato608@gmail.com>
2023-07-06 15:00:10 +0000

    :construction: finished automation

--- TestCron.R
+++ TestCron.R
@@ -2,4 +2,4 @@ library(cronR)
 
 cmd <- cron_rscript("testCronFunc.R")
 
-cron_add(command = cmd, frequency = 'hourly', id = 'test1', at= '00:01', description = 'my process 1')
+cron_add(command = cmd, frequency = 'minutely', id = 'test1', at= '00:01', description = 'my process 1')
--- TestCronFunc.R
+++ TestCronFunc.R
@@ -2,16 +2,16 @@ library(lubridate)
 
 
 
-cond1 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 00:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 00:02:00")))
-cond2 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 04:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 04:02:00")))
-cond3 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 08:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 08:02:00")))
-cond4 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 12:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 12:02:00")))
-cond5 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 16:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 16:02:00")))
-cond6 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 20:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 20:02:00")))
+cond11 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 00:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 00:01:00")))
+cond21 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 04:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 04:01:00")))
+cond31 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 08:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 08:01:00")))
+cond41 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 12:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 12:01:00")))
+cond51 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 16:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 16:01:00")))
+cond61 = (Sys.time() >= as_datetime(paste0(Sys.Date(), " 20:00:00"))) & (Sys.time() < as_datetime(paste0(Sys.Date(), " 20:01:00")))
 
-str1 = readRDS('str1.rds')
 
-if(cond1 | cond2 | cond3 | cond4 | cond5 | cond6){
-  source('/home/rstudio/CryptoWatching/MakePredictions.R')
-  predict.tomorrow.multiple(str1[1:6], '4hour', 0.9)
+if(cond11 | cond21 | cond31 | cond41 | cond51 | cond61){
+  source('C:/Users/xbox/Desktop/Rstuff/CryptoMLClean/TestCronFunc.R')
 }
+
+source('C:/Users/xbox/Desktop/Rstuff/CryptoMLClean/MonitorTPSL.R')
